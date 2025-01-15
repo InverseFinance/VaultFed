003a2dd7dbebed5d47c4443485cae6f15d941576
nourharidy <nourharidy@yahoo.com>
2025-01-15 17:21:06 +0400

    add src

--- script/VaultFedDeployer.s.sol
+++ script/VaultFedDeployer.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {VaultFed} from "../src/VaultFed.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract VaultFedDeployer is Script {
+    VaultFed public vaultFed;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        //vaultFed = new VaultFed();
 
         vm.stopBroadcast();
     }
---
+++ test/VaultFed.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {VaultFed} from "../src/VaultFed.sol";
+
+contract VaultFedTest is Test {
+    VaultFed public vaultFed;
+
+    function setUp() public {
+        //vaultFed = new VaultFed();
+    }
+}
