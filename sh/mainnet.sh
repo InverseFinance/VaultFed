c0d0f324bb1707696461cb52b71f1e09ed7bc4bb
nourharidy <nourharidy@yahoo.com>
2025-02-16 06:27:53 +0400

    mainnet deploy fr

--- sh/mainnet.sh
+++ sh/mainnet.sh
@@ -4,4 +4,4 @@
 source .env
 
 # To deploy and verify our contract
-forge script script/VaultFedDeployer.s.sol --rpc-url $MAINNET_RPC_URL --broadcast -vvvv --etherscan-api-key $ETHERSCAN_API_KEY --verify --slow
+forge script script/VaultFedDeployer.s.sol --rpc-url $MAINNET_RPC_URL --broadcast -vvvv --etherscan-api-key $ETHERSCAN_API_KEY --verify --slow --private-key $PRIVATE_KEY
